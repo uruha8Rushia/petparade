1fa65f1d8142c4685f2ad0d91a39f1d9206b7caf
WeiQing_Cutie <weiqing4288@gmail.com>
2025-01-13 02:29:00 +0800

    edit navbar, add img, add logo

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Import the CSS file for styling
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/product" className="nav-link">
-            Product
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/FAQ" className="nav-link">
-            FAQ
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="header-container">
+      <nav className="navbar">
+        <img src="/nav_logo.png" alt="Logo" className="nav-logo" />
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/product" className="nav-link">
+              Product
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/FAQ" className="nav-link">
+              FAQ
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
