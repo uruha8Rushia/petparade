75e4986acd0e0960079bf055618edf5bf07d7a0f
WeiQing_Cutie <weiqing4288@gmail.com>
2025-01-13 22:53:54 +0800

    edit nav bar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  // Determine if the current page is the home page
+  const isHomePage = location.pathname === "/";
+
   return (
-    <div className="header-container">
+    <div
+      className="header-container"
+      style={{
+        height: isHomePage ? "100vh" : "80px", // Full height on home page, smaller on others
+        backgroundPosition: isHomePage ? "center" : "top",
+        transition: "height 0.3s ease", // Smooth transition
+      }}
+    >
       <nav className="navbar">
         <img src="/nav_logo.png" alt="Logo" className="nav-logo" />
         <ul className="nav-list">
+          {/* Search Bar */}
+          <li className="nav-item search-bar-item">
+            <form className="search-bar">
+              <input
+                type="text"
+                placeholder="Search"
+                className="search-input"
+              />
+              <button type="submit" className="search-button">
+                <i className="fas fa-search"></i>
+              </button>
+            </form>
+          </li>
           <li className="nav-item">
             <Link to="/" className="nav-link">
               Home
@@ -15,7 +40,7 @@ const Navbar = () => {
           </li>
           <li className="nav-item">
             <Link to="/about" className="nav-link">
-              About
+              About Us
             </Link>
           </li>
           <li className="nav-item">
@@ -25,7 +50,7 @@ const Navbar = () => {
           </li>
           <li className="nav-item">
             <Link to="/FAQ" className="nav-link">
-              FAQ
+              FAQs
             </Link>
           </li>
         </ul>
